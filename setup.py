4ba9772977484caa3badd1ced2f7dfa5c9560d1e
Mark Clarkson <mark.clarkson@smorg.co.uk>
2017-04-29 11:28:50 +0100

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     scripts=[
         'JSONPath.sh',
     ],
-    version='0.0.3',
+    version='0.0.5',
     description="JSONPath implementation written in Bash",
     long_description="",
     author='Mark Clarkson',
